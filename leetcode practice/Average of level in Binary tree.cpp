397c6e14436ca8623359afd8ca93d853ce05120c
Arpitshivam Pandey <98251986+Arpit0324@users.noreply.github.com>
2022-09-02 21:42:49 +0530

    Update Average of level in Binary tree.cpp

--- leetcode practice/Average of level in Binary tree.cpp
+++ leetcode practice/Average of level in Binary tree.cpp
@@ -42,3 +42,39 @@ public:
 
 //Time Complexity : O(N), where N is the number of nodes in the given Tree.
 //Space Complexity : O(M), where M is the maximum number of nodes at any level in the binary tree.
+
+
+// using bfs
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+     
+        vector<double>ans;
+        queue<TreeNode*>q;
+        double sum =0;
+        if(!root) return ans;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int levelCount = q.size();
+            
+            for( int i =0; i<levelCount; i++)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                sum += curr->val;
+                
+                if(curr->left)q.push(curr->left);
+                if(curr->right)q.push(curr->right);
+            }
+            
+            double average = 0.0;
+            average = sum/levelCount;
+            ans.emplace_back(average);
+            sum =0;
+        }
+        
+        return ans;
+    }
+};
