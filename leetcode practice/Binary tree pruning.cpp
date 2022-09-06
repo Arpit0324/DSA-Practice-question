14ee0b0f37a3b883b593fd34775774dac0590990
Arpitshivam Pandey <98251986+Arpit0324@users.noreply.github.com>
2022-09-06 21:16:54 +0530

    Add files via upload

--- leetcode practice/Binary tree pruning.cpp
+++ leetcode practice/Binary tree pruning.cpp
@@ -1,7 +1,7 @@
 //Q) Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed. A subtree of a node node is node plus every node that is a descendant of node.
 
 
- 
+
 //if root == null: return null
 //root.left = pruneTree(root.left)
 //root.right = pruneTree(root.right)
