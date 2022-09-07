c1016656225c3cf2c31bbc8451b6f3943f46c0a0
Arpitshivam Pandey <98251986+Arpit0324@users.noreply.github.com>
2022-09-07 15:11:45 +0530

    Update Construct String from Binary Tree.cpp

--- leetcode practice/Construct String from Binary Tree.cpp
+++ leetcode practice/Construct String from Binary Tree.cpp
@@ -26,3 +26,19 @@ public:
         return ans.substr(1,len-2);      // to remove starting and ending brackets
     }
 };
+
+// TC: O(N)
+// SC: O(N)
+
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root)
+            return "";
+        string s= to_string(root->val);
+        if(root->left) s+="(" + tree2str(root->left) +")"; 
+        else if(root->right) s+="()"; // we created parenthesis for left, when it doesnt exist 
+        if(root->right) s+="(" + tree2str(root->right) +")";
+        return s;
+    }
+};
