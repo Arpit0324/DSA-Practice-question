337123597d46d9108e830a8f0121369034c4ed30
Arpitshivam Pandey <98251986+Arpit0324@users.noreply.github.com>
2022-11-22 15:19:28 +0530

    Create My template.cpp

---
+++ My template.cpp
@@ -0,0 +1,35 @@
+// Arpitshivam template 
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define tc(T) int T; cin>>T; while(T--)
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a<b?a:b)
+#define SORT(name) sort(name.begin(), name.end())
+#define fo(i,n) for(i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<=n;i++)
+#define rl(i,m,n) for(int i=n;i>=m;i--)
+// Macros used for code
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef unordered_map<ll,ll> umll;
+typedef map<ll,ll> mll;
+
+
+//Aliases 
+using ll= long long;
+using lld= long double;
+using ull= unsigned long long;
+
+//Constants
+const lld pi= 3.141592653589793238;
+const ll INF= LONG_LONG_MAX;
+const ll mod=1e9+7;
